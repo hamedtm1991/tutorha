890a3ae8936fa2227f47e8aea519dbf6fea3cc15
hamed <hamedmodam1991@gmail.com>
2023-06-25 01:16:00 +0330

    Base of project

---
+++ resources/views/login.blade.php
@@ -0,0 +1,39 @@
+<x-layout>
+    <x-header/>
+    <!-- ============================ Login Wrap ================================== -->
+    <section>
+        <div class="container">
+            <div class="row justify-content-center">
+
+                <div class="col-xl-5 col-lg-8 col-md-12 col-sm-12">
+                    <form>
+                        <div class="crs_log_wrap">
+                            <div class="crs_log__thumb">
+                                <img src="assets/img/banner-2.jpg" class="img-fluid" alt="" />
+                            </div>
+                            <div class="crs_log__caption">
+                                <div class="rcs_log_123">
+                                    <div class="rcs_ico"><i class="fas fa-lock"></i></div>
+                                </div>
+
+                                <div class="rcs_log_124">
+                                    <div class="Lpo09"><h4>ورود به حساب کاربری</h4></div>
+                                    <div class="form-group">
+                                        <label class="mb-1">شماره موبایل</label>
+                                        <input type="text" class="form-control" placeholder="0912xxxxxxx" />
+                                    </div>
+                                    <div class="form-group mt-3 mb-2">
+                                        <button type="button" class="btn full-width btn-md theme-bg text-white">ورود</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- ============================ Login Wrap ================================== -->
+    <x-footer/>
+</x-layout>
