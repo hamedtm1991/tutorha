e8d225cbf5d31242eaa98427fee4f2d6d3d1b5bc
hamed <hamedmodam1991@gmail.com>
2024-07-27 23:50:23 +0330

    Fixing bug 3

--- resources/views/livewire/landings/course.blade.php
+++ resources/views/livewire/landings/course.blade.php
@@ -59,7 +59,7 @@
                                             <ul class="lectures_lists">
                                                 @foreach($group as $episode)
                                                     @php($url = getVideoUrl($episode->links[0] ?? '', $episode))
-                                                    <a style="display: @if(empty($url)) none @endif" class="videourl" episodeid="{{ $episode->id }}" productid="{{ $product->id }}" cover="{{ url(route('getPublicImage', ['episode-' . $episode->id . '-main', rand()])) }}" data-url="{{ $url }}"><li class="complete"><div class="lectures_lists_title"><i class="fas fa-{{ !empty($watchDetail[$episode->id]) ? 'check' : 'play' }} dios"></i></div>{{ $episode->title }}<span class="cls_timing">{{ $episode->time }}</span></li></a>
+                                                    <a style="display: @if(empty($url)) none @endif" class="videourl" episodeid="{{ $episode->id }}" productid="{{ $product->id }}" cover="{{ url(route('getPublicImage', ['Episode-' . $episode->id . '-main', rand()])) }}" data-url="{{ $url }}"><li class="complete"><div class="lectures_lists_title"><i class="fas fa-{{ !empty($watchDetail[$episode->id]) ? 'check' : 'play' }} dios"></i></div>{{ $episode->title }}<span class="cls_timing">{{ $episode->time }}</span></li></a>
 
                                                     @if(!Auth::check() && empty($url))
                                                         <a wire:click="login"><li class="unview"><div class="lectures_lists_title"><i class="fas fa-lock dios"></i></div>{{ $episode->title }}<span class="cls_timing">{{ number_format($episode->price) . ' / ' . $episode->time }}</span></li></a>
