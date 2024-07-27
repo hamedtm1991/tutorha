d1d91ffec1c6e9931801199e9415b152f4c4099b
hamed <hamedmodam1991@gmail.com>
2024-07-28 01:17:56 +0330

    Fixing bug 4

--- resources/views/livewire/landings/course.blade.php
+++ resources/views/livewire/landings/course.blade.php
@@ -50,11 +50,12 @@
                         <h4 class="edu_title">{{ __('general.episodes') }}</h4>
                         <div id="accordionExample" class="accordion shadow circullum">
                             @foreach($episodes as $index => $group)
+                                @php($rand = \Illuminate\Support\Str::random(10))
                                 <div class="card">
-                                    <div id="heading{{ $index }}" class="card-header bg-white shadow-sm border-0">
-                                        <h6 class="mb-0 accordion_title"><a href="#" data-toggle="collapse" data-target="#collapse{{ $index }}" aria-expanded="true" aria-controls="collapse{{ $index }}" class="d-block position-relative text-dark collapsible-link py-2">{{ $index }}</a></h6>
+                                    <div id="heading{{ $rand }}" class="card-header bg-white shadow-sm border-0">
+                                        <h6 class="mb-0 accordion_title"><a href="#" data-toggle="collapse" data-target="#collapse{{ $rand }}" aria-expanded="true" aria-controls="collapse{{ $rand }}" class="d-block position-relative text-dark collapsible-link py-2">{{ $index }}</a></h6>
                                     </div>
-                                    <div id="collapse{{ $index }}" aria-labelledby="heading{{ $index }}" data-parent="#accordionExample" class="collapse show">
+                                    <div id="collapse{{ $rand }}" aria-labelledby="heading{{ $rand }}" data-parent="#accordionExample" class="collapse show">
                                         <div class="card-body pl-3 pr-3">
                                             <ul class="lectures_lists">
                                                 @foreach($group as $episode)
