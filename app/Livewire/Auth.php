95fb5a322ed08a5d5f22600998fdb885a3e3cd5d
hamed <hamedmodam1991@gmail.com>
2024-08-13 00:12:35 +0330

    Fixing bugs

--- app/Livewire/Auth.php
+++ app/Livewire/Auth.php
@@ -75,8 +75,6 @@ class Auth extends Component
         $authService->code = $request['code'];
         $response = $authService->serviceController();
 
-        dd($response);
-
         if (!$response->getData()->status) {
             $this->addError('code', $response->getData()->message);
         }
